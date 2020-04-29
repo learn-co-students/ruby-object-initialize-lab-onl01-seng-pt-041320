6d8e89aae269841047ec6320823fa3081c504c05
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-29 22:47:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  attr_accessor :name, :breed
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name = name
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
