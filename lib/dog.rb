62616cb0b27d8b00cc2499b9e052e728ce9abe4e
Joey <joey.alzamora@gmail.com>
2020-04-29 03:22:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog 
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+# class Dog  
+#   def initialize(breed)    
+#     @breed = breed  
+# end   
+
+# def breed=(breed)    
+#     @breed = breed  
+# end   
+
+# def breed    
+#   @breed  
+#   end 
+# end
---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person 
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+
+# class Dog  
+#   def initialize(breed)    
+#     @breed = breed  
+# end   
+
+# def breed=(breed)    
+#     @breed = breed  
+# end   
+
+# def breed    
+#   @breed  
+#   end 
+# end
