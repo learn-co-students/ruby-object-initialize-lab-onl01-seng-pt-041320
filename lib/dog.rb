b89f369de976cb084bbbc2fbee04b1b65aa95713
Cailie Skelton <cskelton707@gmail.com>
2020-05-01 18:42:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def initalize(name, breed)
+    @name = name
+    @name = breed
+  end
+ def breed
+    @breed
+ end
+  
+  def name
+    @name
+  end
+
+end
+
+fido = Dog.new("Fido", "Pug")
